edd04ebe9b48b3d8462cc55bccc34ddbcc252a26
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2022-09-06 07:19:12 +0600

    k solve

--- Algorithm design and analysis - SWE 229/Coding (by bou)/Sieve contest - 31 august/K no - diff sort.cpp
+++ Algorithm design and analysis - SWE 229/Coding (by bou)/Sieve contest - 31 august/K no - diff sort.cpp
@@ -2,50 +2,62 @@
 
 using namespace std;
 
-const int n = 2*10e6+1;
+const int n = 2000001;
 int i, j;
-vector<int>ans;
+int spf[n];
+
 
 struct node{
 	int num, val;
 };
 
-bool sortt(node a, node b)
-{
-	if(a.val != b.val) return a.val < b.val;
- 	else return a.num < b.num;
-}
-
 node ara[n];
 
-void task()
+bool sortt(node a, node b)
 {
-	ara[0].num = 0, ara[0].val = 0;
-	ara[1].num = 1, ara[1].val = 1;
-	for(j = 2; j < n; j++)
+	if(a.val == b.val) return a.num < b.num;
+	return a.val < b.val;
+}
+
+void sieve()
+{
+	for (i = 1; i < n; i++) 
 	{
-		int cnt = 0;
-		while (j % 2 == 0) j /= 2, cnt++;
-    	for (i = 3; i <= sqrt(j); i += 2) 
-		{	
-			while (j % i == 0) j /= i, cnt++;
-		}
-		if(j < 2) cnt++;
-		ara[i].num = i, ara[i].val = cnt;
+	    if(i % 2 == 0) spf[i] = 2;
+	    else spf[i] = i;
 	}
-	sort(ara, ara+n, sortt);
-	for(i = 0; i < n; i++) cout << ara[i].num << endl;
+	for (i = 3; i * i < n; i += 2)
+	{
+		for (j = i * i; j < n; j += i) if (spf[j] == j) spf[j] = i;
+	}
+	//for (i = 157; i < n; i++) cout << i << " " << spf[i] << endl;
+	for (i = 1; i < n; i++) 
+	{
+	    int cnt = 0;
+	    int x = i;
+    	while (x > 1)
+    	{
+    		cnt++;
+    		x /= spf[x];
+    	}
+    	ara[i].num = i;
+        ara[i].val = cnt;
+        //cout << ara[i].num << " " << ara[i].val << endl;
+	}
+	sort(ara, ara + n, sortt);
+	//for (i = 1; i < n; i++) cout << ara[i].num << " " << ara[i].val << endl;
 }
 
 int main()
 {
-    task();
-    int t;
-    cin >> t;
-    while(t--)
+    sieve();
+    i = 1;
+    while(1)
     {
-        long long N, sq;
+        int N;
 		cin >> N;
-        //cout << v[N-1] << endl;
+		if(N == 0) return 0; 
+        cout << "Case " << i << ": " << ara[N].num << endl;
+        i++;
     }
 }
