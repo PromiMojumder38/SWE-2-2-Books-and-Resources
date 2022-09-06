2972913080360e32aa5aa922c686df12313e23c8
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2022-09-06 20:46:23 +0600

    project idea + algo topics

---
+++ Algorithm design and analysis - SWE 229/Coding (by bou)/Sieve contest - 31 august/I no - prime pair.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define n 1000000000
+vector<ll>sum;
+
+void fun()
+{
+	int i = 1;
+	while(1)
+	{
+		if((p2 * i) % 10 == p1)	sum[]	
+	}
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int p1, p2;
+		cin >> p1 >> p2;
+		
+	}
+}
